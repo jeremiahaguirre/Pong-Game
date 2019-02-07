e37a562127d7daa31468733e2d2749f11af1b6b5
Jeremiah <happytobike@gmail.com>
2019-02-07 15:24:20 -0800

    Added board class to game

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,33 @@
+import { SVG_NS } from "../settings";
+import Board from "./Board";
+// import Paddles from "./Paddles"
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    this.gameElement = document.getElementById(this.element);
+
+    this.board = new Board(this.width, this.height);
+    // this.paddles = new Paddles();
   }
 
   render() {
-		// More code goes here....
+    // empty out game element before re-rendering
+    this.gameElement.innerHTML = "";
+
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    svg.setAttributeNS(null, "version", "1.1");
+
+    this.gameElement.appendChild(svg);
+
+    // rendering all game elements inside the SVG
+    this.board.render(svg);
+    // this.paddles.render(svg);
   }
 }
---
+++ src/partials/Paddles.js
@@ -0,0 +1,12 @@
+ export default class Paddle {
+  constructor(boardHeight, width, height, x, y) {
+    this.boardHeight = boardHeight;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
+    this.score = 0;
+  }
+  
+}
